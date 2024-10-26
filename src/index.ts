a223a82b43f1af89a94113c5ccedf41c86f424d9
2yunseong <56749516+2yunseong@users.noreply.github.com>
2024-10-26 16:16:12 +0900

    fix: 컴포넌트를 import하지 않는 문제 (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import Title from './components/Title/Title';
 import Heading from './components/Heading/Heading';
+import Caption from './components/Caption/Caption';
+import SubTitle from './components/SubTitle/SubTitle';
+import Text from './components/Text/Text';
 import StyleProvider from './components/StyleProvider/StyleProvider';
 import theme from './styles/theme';
 
-export { Title, Heading, StyleProvider, theme };
+
+export { Caption, Title, Heading, SubTitle, Text, StyleProvider, theme };
