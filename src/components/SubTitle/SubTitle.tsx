900485f88dadcf78e94f439098e5f852a64a941f
2yunseong <dbsdltjd123@gmail.com>
2024-11-08 11:02:00 +0900

    feat: Text 컴포넌트에 색상 부여가 가능하게 props 추가

--- src/components/SubTitle/SubTitle.tsx
+++ src/components/SubTitle/SubTitle.tsx
@@ -1,20 +1,36 @@
+import { css, useTheme } from '@emotion/react';
+import { colorAlias } from '../../styles/theme';
+import { useCallback } from 'react';
+
 export interface SubTitleProps extends React.HTMLAttributes<HTMLDivElement> {
   is?: 'sub1' | 'sub2' | 'sub3';
+  color?: keyof typeof colorAlias;
 }
 
-const SubTitle = ({ children, is = 'sub1', ...props }: SubTitleProps) => {
+const SubTitle = ({
+  children,
+  is = 'sub1',
+  color = 'black',
+  ...props
+}: SubTitleProps) => {
+  const theme = useTheme();
+
+  const getSubTitleStyle = useCallback(() => {
+    switch (is) {
+      case 'sub2':
+        return theme.font.subtitle2;
+      case 'sub3':
+        return theme.font.subtitle3;
+      default:
+        return theme.font.subtitle1;
+    }
+  }, [is, theme.font.subtitle1, theme.font.subtitle2, theme.font.subtitle3]);
   return (
     <h6
-      css={(theme) => {
-        switch (is) {
-          case 'sub2':
-            return theme.font.subtitle2;
-          case 'sub3':
-            return theme.font.subtitle3;
-          default:
-            return theme.font.subtitle1;
-        }
-      }}
+      css={css`
+        ${getSubTitleStyle()}
+        color: ${colorAlias[color]};
+      `}
       {...props}
     >
       {children}
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,29 +1,40 @@
+import { css, useTheme } from '@emotion/react';
+import { colorAlias } from '../../styles/theme';
+import { useCallback } from 'react';
+
 export interface TextProps
   extends React.HTMLAttributes<
     HTMLParagraphElement | HTMLDivElement | HTMLSpanElement
   > {
   is?: 'b1' | 'b2' | 'b3';
   as?: 'p' | 'div' | 'span';
+  color?: keyof typeof colorAlias;
 }
 
 const Text = ({
   children,
   as: Component = 'p',
   is = 'b1',
+  color = 'black',
   ...props
 }: TextProps) => {
+  const theme = useTheme();
+  const getTextStyle = useCallback(() => {
+    switch (is) {
+      case 'b2':
+        return theme.font.body2;
+      case 'b3':
+        return theme.font.body3;
+      default:
+        return theme.font.body1;
+    }
+  }, [is, theme.font.body1, theme.font.body2, theme.font.body3]);
   return (
     <Component
-      css={(theme) => {
-        switch (is) {
-          case 'b2':
-            return theme.font.body2;
-          case 'b3':
-            return theme.font.body3;
-          default:
-            return theme.font.body1;
-        }
-      }}
+      css={css`
+        ${getTextStyle()}
+        color: ${colorAlias[color]};
+      `}
       {...props}
     >
       {children}
